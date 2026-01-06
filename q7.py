029134421af87d6b6a2626be60a3fdcc21e73103
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-01-06 12:12:38 +0200

    q6

---
+++ q7.py
@@ -0,0 +1,7 @@
+"""7. Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional
+array. (Where X: number of raws, Y: number of columns).The element value in the i-th
+row and j-th column of the array should be i*j.
+Input:
+3,5
+Expected output:
+[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]"""
