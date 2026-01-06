5cb5d70fb95e822c9b358d1f70bc89b2319f54e2
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-01-06 12:33:56 +0200

    more q

--- q7.py
+++ q7.py
@@ -5,3 +5,14 @@ Input:
 3,5
 Expected output:
 [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]"""
+
+x = int(input("Enter number of rows: "))
+y = int(input("Enter number of columns: "))
+array=[]
+for i in range(x):
+    row=[]
+    for j in range (y):
+        row.append(i*j)
+    
+    array.append(row)
+print(array)
---
+++ q8.py
@@ -0,0 +1,12 @@
+"""Write a program to print the following pattern using 2 inputs (X,Y) from the user.
+Hint: assume a 2D matrix of X raws and Y columns (Where X: number of raws, Y:
+number of columns).The element value in the i-th row and j-th column of the array
+should be i*j.
+Input:
+3,5
+Expected output:
+0 0 0 0 0
+0 1 2 3 4
+0 2 4 6 8
+"""
+
